6df7218df2aa35c04ae826e2603fd98babfdf78c
Cesar Cardenas <cesar.cardam@gmail.com>
2018-01-27 17:42:14 -0600

    test7

--- chat-integration/fbchatintegration/routes/index.js
+++ chat-integration/fbchatintegration/routes/index.js
@@ -119,15 +119,26 @@ router.post('/webhook', (req, res) => {
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+  body.entry.forEach(function(entry) {
 
-      // Get the webhook event. entry.messaging is an array, but
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+    // Gets the body of the webhook event
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
 
-    });
+
+    // Get the sender PSID
+    let sender_psid = webhook_event.sender.id;
+    console.log('Sender PSID: ' + sender_psid);
+
+    // Check if the event is a message or postback and
+    // pass the event to the appropriate handler function
+    if (webhook_event.message) {
+      handleMessage(sender_psid, webhook_event.message);
+    } else if (webhook_event.postback) {
+      handlePostback(sender_psid, webhook_event.postback);
+    }
+
+  });
 
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
