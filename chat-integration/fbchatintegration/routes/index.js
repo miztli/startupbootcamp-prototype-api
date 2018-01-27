7dd6b5e28beda11436ad1e8a867ce41357b71286
Cesar Cardenas <cesar.cardam@gmail.com>
2018-01-27 17:54:57 -0600

    test11

--- chat-integration/fbchatintegration/routes/index.js
+++ chat-integration/fbchatintegration/routes/index.js
@@ -31,13 +31,14 @@ function handlePostback(sender_psid, received_postback) {
 
 function callSendAPI(sender_psid, response) {
   // Construct the message body
+  console.log("one");
   let request_body = {
     "recipient": {
       "id": sender_psid
     },
     "message": response
   }
-
+  console.log("two");
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
@@ -51,6 +52,7 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   });
+  console.log("three");
 }
 
 //Watson
