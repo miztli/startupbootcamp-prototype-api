5da6de61fe6afa81e98d75a7300a150e799ac81f
Cesar Cardenas <cesar.cardam@gmail.com>
2018-01-27 17:11:55 -0600

    test2

--- chat-integration/fbchatintegration/routes/index.js
+++ chat-integration/fbchatintegration/routes/index.js
@@ -110,28 +110,30 @@ router.get('/webhook', (req, res) => {
 });
 
 // Creates the endpoint for our webhook
-router.post('/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {
 
+  // Parse the request body from the POST
   let body = req.body;
 
-  // Checks this is an event from a page subscription
-  body.entry.forEach(function(entry) {
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
 
-  // Gets the body of the webhook event
-  let webhook_event = entry.messaging[0];
-  console.log(webhook_event);
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
+      // Get the webhook event. entry.messaging is an array, but
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
 
-  // Get the sender PSID
-  let sender_psid = webhook_event.sender.id;
-  console.log('Sender PSID: ' + sender_psid);
+    });
 
-  // Check if the event is a message or postback and
-  // pass the event to the appropriate handler function
-  if (webhook_event.message) {
-    handleMessage(sender_psid, webhook_event.message);
-  } else if (webhook_event.postback) {
-    handlePostback(sender_psid, webhook_event.postback);
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
 
 });
