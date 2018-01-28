1762ee494042b458892111739d0b64c8182dc14b
Cesar Cardenas <cesar.cardam@gmail.com>
2018-01-27 21:02:02 -0600

    test author

--- chat-integration/fbchatintegration/routes/index.js
+++ chat-integration/fbchatintegration/routes/index.js
@@ -7,6 +7,38 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function receivedMessageStore(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, "repito: "+messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
 
 function receivedMessage(event) {
   var senderID = event.sender.id;
@@ -207,6 +239,7 @@ router.post('/webhook', (req, res) => {
           let isEcho=message.is_echo;
           if(isEcho){
             console.log(message);
+            receivedMessageStore(event);
           }
           else {
             receivedMessage(event);
