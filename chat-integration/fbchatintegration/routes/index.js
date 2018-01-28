5e132e216ea1caf9045c0c0d96b858453d3fb3e8
Cesar Cardenas <cesar.cardam@gmail.com>
2018-01-27 19:36:32 -0600

    test x

--- chat-integration/fbchatintegration/routes/index.js
+++ chat-integration/fbchatintegration/routes/index.js
@@ -117,6 +117,8 @@ function sendTextMessage(recipientId, messageText) {
 }
 
 function callSendAPI(messageData) {
+  console.log(messageData);
+  /*
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: PAGE_ACCESS_TOKEN },
@@ -136,6 +138,7 @@ function callSendAPI(messageData) {
       console.error(error);
     }
   });
+  */
 }
 
 
