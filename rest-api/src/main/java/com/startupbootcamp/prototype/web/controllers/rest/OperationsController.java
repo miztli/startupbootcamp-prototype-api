f0aa04c7ffd53b527812b1da58b0c604c0bd0e6d
Miztli Melgoza <miztli.melgoza@conecta.co>
2018-01-28 09:47:39 -0600

    Add error and success page

--- rest-api/src/main/java/com/startupbootcamp/prototype/web/controllers/rest/OperationsController.java
+++ rest-api/src/main/java/com/startupbootcamp/prototype/web/controllers/rest/OperationsController.java
@@ -3,6 +3,7 @@ package com.startupbootcamp.prototype.web.controllers.rest;
 import com.startupbootcamp.prototype.service.OperationsService;
 import finlab.damain.model.BalanceResponse;
 import finlab.damain.model.GenericResponse;
+import finlab.damain.model.TransferResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,14 +24,5 @@ public class OperationsController {
         return operationsService.balance(account);
     }
 
-    //test: http://localhost:8080/transfer?from=00113183315&to=00113183316&amount=1&reference=pruebachrome
-    @RequestMapping(value = "/transfer")
-    @ResponseStatus(HttpStatus.OK)
-    public GenericResponse transfer(@RequestParam(name = "from") final String from,
-                                    @RequestParam(name = "to") final String to,
-                                    @RequestParam(name = "amount") final String amount,
-                                    @RequestParam(name = "reference") final String reference){
-        System.out.println("----Getting account balance----");
-        return operationsService.transfer(from,to,amount,reference);
-    }
+
 }
