6bd913ddbb55ae61f94a6019d435633789bf19b5
Cesar Cardenas <cesar.cardam@gmail.com>
2018-01-27 18:54:59 -0600

    new test

--- chat-integration/fbchatintegration/routes/index.js
+++ chat-integration/fbchatintegration/routes/index.js
@@ -38,6 +38,7 @@ function callSendAPI(sender_psid, response) {
   }
   console.log(PAGE_ACCESS_TOKEN);
   // Send the HTTP request to the Messenger Platform
+  /*
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
     "qs": { "access_token": PAGE_ACCESS_TOKEN },
@@ -50,6 +51,7 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   });
+  */
 }
 
 //Watson
